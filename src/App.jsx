08aec3a1ebb5ace8fdb14a98d2111c131ed28766
cyrus_w <cyrusw89@gmail.com>
2024-06-01 19:52:08 +1200

    added base wireframe and css, input forms that posts to /aichat endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import AIInterviewer from "./components/AIInterviewer"
 
 //Note - use import.meta.env.VITE_REACT_APP_BACKEND_URL for the endpoint - this is saved in GitHub secrets
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <AIInterviewer />
+
     </>
   )
 }
