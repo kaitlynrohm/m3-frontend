ce0d5775bef8700dbe6e6bff5ef979292e9f092d
Nicole Gunn <nicole@clockworkproperty.co.nz>
2024-07-01 07:05:23 +1200

    updated package.json for issue running npm run dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,15 @@
-<<<<<<< HEAD
-import AIInterviewer from "./components/AIInterviewer.jsx"
-=======
-import './App.css'
-// import AIInterviewer from "./components/AIInterviewer.jsx"
-import AIInsurance from "./components/AIInsurance.jsx"
-import TCnav from "./assets/TCnav.png"
->>>>>>> 31a363bdca00f32d867696daf12ceab34d3366b8
-
-//Note - use import.meta.env.VITE_REACT_APP_BACKEND_URL for the endpoint - this is saved in GitHub secrets
+import AIInsurance from "./components/AIInsurance.jsx";
+import AIInterviewer from "./components/AIInterviewer.jsx";
+import TCnav from "./assets/TCnav.png";
 
 function App() {
-
   return (
-    
-    <div className='body'>
-        <img className='nav' src={TCnav} alt="nav" />
-        {/* <AIInterviewer /> */}
-        <AIInsurance />
-
+    <div className="body">
+      {/* <img className="nav" src={TCnav} alt="nav" /> */}
+      <AIInsurance />
+      {/* <AIInterviewer /> */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
